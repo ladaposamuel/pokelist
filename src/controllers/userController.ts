802e9eb6a3670fb33f96156866cca26ef5c8b0c7
Samuel Ladapo <ladaposamuel@outlook.com>
2024-08-03 22:13:45 +0100

    (feat) - user login and register

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,25 +1,54 @@
-import { Controller, Get, Middleware } from '@overnightjs/core';
+import { Controller, Get, Post, Middleware } from '@overnightjs/core';
 import type { Request, Response } from 'express';
-// import { NotAuthenticated } from '../exceptions';
-// import { type IJwt, isAuthenticated } from '../middleware/auth';
+
+import { type IJwt, isAuthenticated } from '../middleware/auth';
 import { UserService } from '../services/userService';
-import { handleServiceResponse } from '../util/httpHandlers';
+import { AuthService } from '../services/AuthService';
+import { handleServiceResponse, validateRequest } from '../util/httpHandlers';
+import { ServiceResponse } from '../util/serviceResponse';
+import { registerUserSchema, loginUserSchema } from '../schemas/userSchemas';
 
 @Controller('api/user')
 export class Users {
-  @Get('')
-  //   @Middleware([isAuthenticated])
+  @Get('me')
+  @Middleware([isAuthenticated])
   public async me(req: Request, res: Response) {
-    //const auth = res.locals.auth as IJwt;
+    const auth = res.locals.auth as IJwt;
+    const userId = auth.userId;
 
-    // const me = await UserService.id(auth.userId);
+    const me = await UserService.id(userId);
 
-    // if (!me) {
-    //   throw new NotAuthenticated();
-    // }
-
-    const me = await UserService.id(133);
+    if (!me) {
+      return handleServiceResponse(
+        ServiceResponse.failure('User not found', null, 404),
+        res
+      );
+    }
 
     return handleServiceResponse(me, res);
+  }
+
+  @Post('register')
+  @Middleware([validateRequest(registerUserSchema)])
+  public async register(req: Request, res: Response) {
+    const { name, email, password, organisationId } = req.body;
+
+    const user = await AuthService.register({
+      name,
+      email,
+      password,
+      organisationId,
+    });
+    return handleServiceResponse(user, res);
+  }
+
+  @Post('login')
+  @Middleware([validateRequest(loginUserSchema)])
+  public async login(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    const user = await AuthService.login({ email, password });
+
+    return handleServiceResponse(user, res);
   }
 }
